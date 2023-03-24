1df9ce89fb95dc061fb34605b1cf48edf749a4fb
liaodkill0017 <96228383+liaodkill0017@users.noreply.github.com>
2023-03-24 16:58:32 +0700

    Create ca_mau_universty.js

---
+++ universities/ca_mau_universty.js
@@ -0,0 +1,6 @@
+const bac_lieu_university = { 
+    title: "Trường Đại học Bình Dương - Cà Mau",
+	address: '3 Lê Thị Riêng, Phường 5, Thành phố Cà Mau, Cà Mau 970000',
+    longitude: 105.1668699, 
+    latitude: 9.186012449909226
+};
